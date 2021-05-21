faf202910e831d9f06c258b75fe130f30e63ff73
romanes13 <43137236+romanes13@users.noreply.github.com>
2021-05-21 14:59:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    if (principal<=0 || isNaN(principal)==True ){
+    if (principal<=0){
         window.alert("Enter a positive number");
         document.getElementById("principal").focus()
         return false;
